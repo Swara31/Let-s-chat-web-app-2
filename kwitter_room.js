09e85f78d3dc77647c71a8fd6a7f3b68ea4a2627
Swara Doiphode <85820539+Swara31@users.noreply.github.com>
2022-02-11 18:51:04 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCf7wRAgUdRSTyYjzad_W88RLajAko7-qo",
+    authDomain: "project-94-b2802.firebaseapp.com",
+    projectId: "project-94-b2802",
+    storageBucket: "project-94-b2802.appspot.com",
+    messagingSenderId: "851233955368",
+    appId: "1:851233955368:web:b71e714003de0424de5f1d"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
